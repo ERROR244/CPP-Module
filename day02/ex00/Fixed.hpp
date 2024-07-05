8313f004f4f5ad5f41f85f81289787f844fc5751
ERROR244 <khalilsohail24@gmail.com>
2024-07-05 09:42:41 +0100

    start day02.

---
+++ day02/ex00/Fixed.hpp
@@ -0,0 +1,11 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <fstream> 
+#include <iostream>
+#include <sstream> 
+#include <string>
+
+
+
+#endif
---
+++ day02/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "Fixed.hpp"
+
+int main(int ac, char **av)
+{
+    
+    return 0;
+}
