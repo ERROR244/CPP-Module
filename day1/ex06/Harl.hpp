3e3b7ca446a9233d859d4951f778c8ba51a2d977
ERROR244 <khalilsohail24@gmail.com>
2024-07-05 08:51:19 +0100

    start ex06.

---
+++ day1/ex06/Harl.hpp
@@ -0,0 +1,19 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <fstream> 
+#include <iostream>
+#include <sstream> 
+#include <string>
+
+class Harl {
+    public:
+        void complain( std::string level );
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+} ;
+
+#endif
---
+++ day1/ex06/main.cpp
@@ -0,0 +1,9 @@
+#include "Harl.hpp"
+
+int main(int ac, char **av)
+{
+    Harl harl;
+    
+
+    return 0;
+}
