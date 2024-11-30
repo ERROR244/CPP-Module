2ae033933f55f6f4e7e23761bddaef59a736e1b2
ERROR244 <khalilsohail24@gmail.com>
2024-11-30 19:36:52 +0100

    CPP06/ex02 Started.

---
+++ CPP06/ex02/include/func.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   func.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksohail- <ksohail-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 19:33:33 by ksohail-          #+#    #+#             */
+/*   Updated: 2024/11/30 19:34:47 by ksohail-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FUNC_HPP
+#define FUNC_HPP
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void);
+void identify(Base* p);
+void identify(Base& p);
+
+#endif
